e007b3f58bb407d1781c6b74e13c09a62afa4897
Ayush Jain <ayushjai@iitg.ac.in>
2020-06-07 13:56:33 +0530

    Update settings.py

--- event/settings.py
+++ event/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'yav%gc-5h4c3za9a6_d1m@9f(yl0t15kir6i-nz_91s!leln1l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['stackhackers.herokuapp.com']
 
 
 # Application definition
